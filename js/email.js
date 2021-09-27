8e2e5a03b32f201c3e45ef6779319cb706c59cb0
Tim Stumper <tjstumpy@udel.edu>
2021-09-27 14:34:35 -0400

    added .gitignore and updated input validation

--- js/email.js
+++ js/email.js
@@ -13,8 +13,6 @@ window.onload = function(){
             console.log('FAILED...', error);
         });
         console.log("input is valid!");
-        }else{
-
         }
 
     });
@@ -29,13 +27,13 @@ function isValidInput(){
     var user_message = document.getElementById("user_message").value;
 
     if(user_fname == "" || user_lname == ""){ //validate names
-        console.log("first/last name is empty!");
+        alert("First/Last name is required!");
         return false;
     }else if(!user_phone.match(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/)){ //validate phone number. found regex from https://www.w3resource.com/javascript/form/phone-no-validation.php
-        console.log("number format is wrong!");
+        alert("Number format is wrong!\n\nAcceptable Formats:\n000 000 0000\n000-000-0000\n000.000.0000");
         return false;
     }else if(user_message == ""){
-        console.log("Message is empty! Message: " + user_message);
+        alert("Please enter a message.");
         return false;
     }
 
